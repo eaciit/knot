fb2f6b9eb5fc038acb3d5e25d6ac7739b18ccb4b
juragan360 <ariefda@hotmail.com>
2015-12-25 06:25:27 +0700

    add some todos on sharedobject

--- knot.v1/sharedobject.go
+++ knot.v1/sharedobject.go
@@ -1,7 +1,10 @@
 package knot
 
+//--- please remove this, do manual checking instead to reduce memory cost allocation
 import "sync"
 
+//--- please use M for data. M already has all method requried
+//--- change variable name to sharedObject to make it not exposable to outside
 type SharedObject struct {
 	data map[string]interface{}
 }
@@ -9,7 +12,9 @@ type SharedObject struct {
 var instance *SharedObject
 var once sync.Once
 
+//--- change to SharedObject
 func GetSharedObject() *SharedObject {
+	//--- use check if nil
 	once.Do(func() {
 		instance = &SharedObject{}
 		instance.data = map[string]interface{}{}
@@ -18,6 +23,7 @@ func GetSharedObject() *SharedObject {
 	return instance
 }
 
+//-- will be handled by M
 func (s *SharedObject) Get(key string) interface{} {
 	if data, isExist := s.data[key]; isExist {
 		return data
@@ -26,6 +32,7 @@ func (s *SharedObject) Get(key string) interface{} {
 	return nil
 }
 
+//-- will be handled by M
 func (s *SharedObject) GetWithDefaultValue(key string, defaultValue interface{}) interface{} {
 	if data, isExist := s.data[key]; isExist {
 		return data
@@ -34,10 +41,12 @@ func (s *SharedObject) GetWithDefaultValue(key string, defaultValue interface{})
 	return defaultValue
 }
 
+//-- will be handled by M
 func (s *SharedObject) Set(key string, value interface{}) {
 	s.data[key] = value
 }
 
+//-- not neccesarily needed, can access knot.SharedObject() directly
 func (s *SharedObject) All() map[string]interface{} {
 	return s.data
 }
